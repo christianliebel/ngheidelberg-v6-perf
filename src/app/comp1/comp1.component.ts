3128a759bf27bd6229737ad58d38d10d844002ef
Christian Liebel <christian.liebel@thinktecture.com>
2019-07-04 17:35:35 +0200

    Mousemove handler

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comp1',
@@ -10,6 +10,10 @@ export class Comp1Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener('document:mousemove')
+  onMouseMove() {
   }
 
 }
