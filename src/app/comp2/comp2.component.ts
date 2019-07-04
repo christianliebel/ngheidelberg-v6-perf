2daa17c8be12c1b4ff9cd34f84819bb250d9d64b
Christian Liebel <christian.liebel@thinktecture.com>
2019-07-04 17:22:32 +0200

    Demo 1/2

---
+++ src/app/comp2/comp2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  templateUrl: './comp2.component.html',
+  styleUrls: ['./comp2.component.css']
+})
+export class Comp2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get foo() {
+    console.log('foo read!');
+    return 'foo';
+  }
+
+}
